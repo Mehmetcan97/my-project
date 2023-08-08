bba22a2c4c5b3e3f6d8a626e6f1b6300e3c479de
Mehmetcan <104503876+Mehmetcan97@users.noreply.github.com>
2023-08-08 14:59:51 +0300

    Github Actions

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/mehmetcan/Documents/Nuxi/App/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Second App</h1>
+    <ul>
+       
+      <li><nuxt-link to="/page1">Page 1</nuxt-link></li>
+      <li><nuxt-link to="/page2">Page 2</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    let pageTitle = 'Second App';
+      if (this.$route && this.$route.path === '/page1') {
+      pageTitle = 'Page 1';
+    } else if (this.$route && this.$route.path === '/page2') {
+      pageTitle = 'Page 2';
+    }
+
+    return {
+      title: pageTitle
+    };
+  },
+
+  mounted() {
+    if (this.$route.path === '/page1') {
+      window.location.href = 'https://www.google.com';
+    }
+    if (this.$route.path === '/page2') {
+      window.location.href = 'https://nuxt.com/docs/getting-started/installation';
+    }
+  }
+};
+</script>
